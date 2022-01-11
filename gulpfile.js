3dd75b8c0041508a417f77d797e765f13a8b0020
CodeBlack29 <39704797+vegas29@users.noreply.github.com>
2022-01-11 13:33:32 -0500

    Header realizado

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,60 @@
-const { src, dest, watch, series } = require('gulp');
+const {src, dest, watch, series} = require('gulp');
 
-// CSS y SASS
+
+//CSS y SASS
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const cssnano = require('cssnano');
 
-// Imagenes
+//Imagenes
+
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
-function css( done ) {
+function css( done ){
+    //Compilar el sass
+
+    //Identificar el archivo, Compilarla, Guardar el archivo .scss
     src('src/scss/app.scss')
-        .pipe( sourcemaps.init() )
-        .pipe( sass() )
-        .pipe( postcss([ autoprefixer(), cssnano() ]) )
-        .pipe( sourcemaps.write('.'))
-        .pipe( dest('build/css') )
+        .pipe( sourcemaps.init())
+        .pipe( sass())
+        .pipe( postcss( [autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe( dest('build/css'))
 
     done();
 }
 
-function imagenes() {
-    return src('src/img/**/*')
-        .pipe( imagemin({ optimizationLevel: 3 }) )
-        .pipe( dest('build/img') )
+function imagenes(){
+    return src('src/imgs/**/*')
+        .pipe( imagemin({optimizationLevel: 3}))
+        .pipe(dest('build/img'));   
 }
 
-function versionWebp() {
+function versionWebp(){
     const opciones = {
         quality: 50
     }
     return src('src/img/**/*.{png,jpg}')
-        .pipe( webp( opciones ) )
-        .pipe( dest('build/img') )
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
 }
-function versionAvif() {
+
+function versionAvif(){
     const opciones = {
         quality: 50
     }
     return src('src/img/**/*.{png,jpg}')
-        .pipe( avif( opciones ) )
-        .pipe( dest('build/img'))
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'))
 }
 
-function dev() {
-    watch( 'src/scss/**/*.scss', css );
-    watch( 'src/img/**/*', imagenes );
+function dev(){
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes);
 }
 
 
@@ -57,4 +63,4 @@ exports.dev = dev;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.default = series(css, dev  );
+exports.default = series(imagenes, css, dev);
