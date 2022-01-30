486dc709be37e7858fae322d68df39cb62f900db
CodeBlack29 <39704797+vegas29@users.noreply.github.com>
2022-01-30 14:03:41 -0500

    Terminao

--- build/js/app.js
+++ build/js/app.js
@@ -6,3 +6,10 @@ function openNav() {
 function closeNav() {
   document.querySelector("#myNav").style.width = "0%";
 }
+
+/*Interfaz nocturna*/
+
+function nochePaz(){
+  document.querySelector('body').classList.toggle("bodyNight");
+  document.querySelectorAll('.menu__item')[1].classList.toggle("sun");
+}
--- src/js/app.js
+++ src/js/app.js
@@ -7,3 +7,4 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
 }
+
