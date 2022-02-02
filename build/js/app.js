0058a9cf85a4e70a4daba69f87ff29e922d83900
CodeBlack29 <39704797+vegas29@users.noreply.github.com>
2022-02-02 08:38:53 -0500

    Corrigiendo algunos imports y detalles css

--- build/js/app.js
+++ build/js/app.js
@@ -13,3 +13,26 @@ function nochePaz(){
   document.querySelector('body').classList.toggle("bodyNight");
   document.querySelectorAll('.menu__item')[1].classList.toggle("sun");
 }
+
+/* Modal Imagenes*/
+
+let modal = document.getElementById("myModal");
+
+let img = document.getElementById("myImg1");
+let modalImg = document.getElementById("img01");
+let captionText = document.getElementById("caption");
+
+let imagensrc = document.querySelectorAll('.portfolio__button')[11].parentNode.firstElementChild.firstElementChild.alt;
+let imagenalt = document.querySelectorAll('.portfolio__button')[11].parentNode.firstElementChild.firstElementChild.alt;
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = imagensrc;
+  captionText.innerHTML = imagenalt;
+}
+
+let span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
